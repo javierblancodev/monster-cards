d57d1beb25b90e4eb5dc2704894e975ec3ee5e40
Javier Blanco <javierblancoit@gmail.com>
2023-04-22 18:07:10 +0100

    format code

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import Card from '../card/card.component';
-import './card-list.styles.css'
+import './card-list.styles.css';
 
 class CardList extends Component {
   // the constructor is still present (and super() called) but we do not write it down since we do not need to overwrite with an initial state for example
@@ -8,23 +8,18 @@ class CardList extends Component {
   // same thing apply to componentDidMount: would run after the render method if we specify it
 
   render() {
-    const {monsters} = this.props
+    const { monsters } = this.props;
 
     return (
-      <div className='card-list'>
+      <div className="card-list">
         {monsters.map((monster) => {
-          const { id } = monster
+          const { id } = monster;
 
-          return (
-            <Card 
-              key={id}
-              monster={monster}
-            />
-          );
+          return <Card key={id} monster={monster} />;
         })}
       </div>
-    )
+    );
   }
 }
 
-export default CardList
+export default CardList;
