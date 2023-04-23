a04bddef4f1f0b5eac0e59289fd213d7d7eb0f9c
Javier Blanco <javierblancoit@gmail.com>
2023-04-23 16:28:57 +0100

    move rest of components from class type to functional type

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,25 +1,14 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-  // the constructor is still present (and super() called) but we do not write it down since we do not need to overwrite with an initial state for example
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      const { id } = monster;
 
-  // same thing apply to componentDidMount: would run after the render method if we specify it
-
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          const { id } = monster;
-
-          return <Card key={id} monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+      return <Card key={id} monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
